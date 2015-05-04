c7dc12cd0d44ad1b0ba2f6869a423f06647884ef
Mark Harrison <mark@mivok.net>
2015-05-04 11:53:43 -0400

    Fix tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'chef-grafana::default' do
-  describe service('grafana') do
+  describe service('grafana-server') do
     it { should be_enabled }
     it { should be_running }
   end
@@ -21,7 +21,7 @@ describe 'chef-grafana::default' do
   describe file('/etc/grafana/grafana.ini') do
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
-    it { should be_mode 0644 }
+    it { should be_mode 644 }
     it { should contain '# Managed by Chef' }
   end
 end

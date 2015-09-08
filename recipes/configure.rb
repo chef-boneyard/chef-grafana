80bd82fe32dcebf9017e922f415561a7f6f49134
Mark Harrison <mark@mivok.net>
2015-09-08 15:21:09 -0400

    Create data/log dirs if we change them from defaults

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -5,3 +5,18 @@ template "/etc/grafana/grafana.ini" do
   variables :config => node['chef-grafana']['config']
   notifies :restart, "service[grafana-server]"
 end
+
+# Make data/log dirs if we changed them from the defaults
+directory node['chef-grafana']['config']['paths']['data'] do
+  owner "grafana"
+  group "grafana"
+  mode "0755"
+  recursive true
+end
+
+directory node['chef-grafana']['config']['paths']['logs'] do
+  owner "grafana"
+  group "grafana"
+  mode "0755"
+  recursive true
+end

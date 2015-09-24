b28fa52a0530ee09fbdc9b1285e7a188904a7489
Tim Smith <tsmith84@gmail.com>
2015-09-24 09:37:14 -0700

    Relicense to Apache 2 to match our cookbooks Anything in this repo is Apache 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,16 @@
 name "chef-grafana"
-description "Installs/Configures grafana"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 maintainer "Mark Harrison"
 maintainer_email "mharrison@chef.io"
-license "MIT"
+license 'Apache 2.0'
+description "Installs/Configures grafana"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version "0.2.1"
 
-source_url "https://github.com/mivok/chef-grafana"
+%w(ubuntu debian).each do |os|
+  supports os
+end
 
+source_url "https://github.com/chef-cookbooks/chef-grafana"
+issues_url 'https://github.com/chef-cookbooks/chef-grafana/issues' if respond_to?(:issues_url)
 depends "apt"

a55e43c65f42ac25e7d70d42687f4c3b98e7b458
Stefan Sundin <stefan.sundin@outreach.io>
2017-12-15 15:13:55 -0800

    Add unit test to install a specific version.
    Signed-off-by: Stefan Sundin <stefan.sundin@outreach.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,7 +7,29 @@ describe 'default recipe on Ubuntu 14.04' do
     end.converge('chef-grafana::default')
   end
 
-  it 'converges successfully' do
-    expect { :chef_run }.to_not raise_error
+  let(:chef_run_versioned) do
+    ChefSpec::ServerRunner.new do |node|
+      node.automatic['chef-grafana']['install']['version'] = '4.5.0'
+    end.converge('chef-grafana::default')
+  end
+
+  context 'latest version' do
+    it 'converges successfully' do
+      expect { :chef_run }.to_not raise_error
+    end
+
+    it 'upgrades successfully' do
+      expect(chef_run).to upgrade_package('grafana')
+    end
+  end
+
+  context 'specific version' do
+    it 'converges successfully' do
+      expect { :chef_run_versioned }.to_not raise_error
+    end
+
+    it 'installs successfully' do
+      expect(chef_run_versioned).to install_package('grafana').with(version: '4.5.0')
+    end
   end
 end
